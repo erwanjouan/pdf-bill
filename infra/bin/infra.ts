35ee8312b81ba09df9746ce3e674b2c8ba7d1aad
Erwan Jouan <erwan.jouan@theatomicity.com>
2025-12-07 22:05:29 +0100

    github actions

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import {PdfBillStack} from '../lib/pdf-bill-stack';
 
 const app = new cdk.App();
-new PdfBillStack(app, 'PdfBillStack', {
+new PdfBillStack(app, 'pdf-bill-stack', {
     /* If you don't specify 'env', this stack will be environment-agnostic.
      * Account/Region-dependent features and context lookups will not work,
      * but a single synthesized template can be deployed anywhere. */
