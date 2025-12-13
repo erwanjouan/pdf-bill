cba55473143d231b2fe6846135717198e8a1e8ae
Erwan Jouan <erwan.jouan@theatomicity.com>
2025-12-13 09:54:00 +0100

    user pool

--- infra/lib/pdf-bill-stack.ts
+++ infra/lib/pdf-bill-stack.ts
@@ -3,6 +3,8 @@ import {Duration} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {Bucket} from "aws-cdk-lib/aws-s3";
 import {ManagedPolicy} from "aws-cdk-lib/aws-iam";
+import {UserPool, UserPoolClient, OAuthScope} from "aws-cdk-lib/aws-cognito";
+import {CognitoUserPoolsAuthorizer} from "aws-cdk-lib/aws-apigateway";
 
 const lambda = require('aws-cdk-lib/aws-lambda');
 const apigw = require('aws-cdk-lib/aws-apigateway');
@@ -13,6 +15,22 @@ export class PdfBillStack extends cdk.Stack {
         super(scope, id, props);
 
         const myBucket = new Bucket(this, 'myBucket');
+
+        // Cognito User Pool for OAuth2 Client Credentials
+        const userPool = new UserPool(this, 'PdfBillUserPool', {
+            userPoolName: 'pdf-bill-user-pool'
+        });
+
+        const userPoolClient = new UserPoolClient(this, 'PdfBillUserPoolClient', {
+            userPool,
+            generateSecret: true,
+            oAuth: {
+                flows: {
+                    clientCredentials: true
+                },
+                scopes: [OAuthScope.custom('pdf-bill/generate')]
+            }
+        });
 
         const myLambda = new lambda.Function(this, 'myFunction', {
             code: lambda.Code.fromAsset('../target/pdf-bill-1.0-SNAPSHOT.jar'),
@@ -29,14 +47,32 @@ export class PdfBillStack extends cdk.Stack {
 
         myLambda.role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"))
 
-        const endpoint = new apigw.LambdaRestApi(this, 'myEndpoint', {
-            handler: myLambda,
-            restApiName: "PdfBillApi"
+        const authorizer = new CognitoUserPoolsAuthorizer(this, 'PdfBillAuthorizer', {
+            cognitoUserPools: [userPool]
+        });
+
+        const api = new apigw.RestApi(this, 'PdfBillApi', {
+            restApiName: 'PdfBillApi'
+        });
+
+        const pdfBillResource = api.root.addResource('pdfbill');
+        pdfBillResource.addMethod('POST', new apigw.LambdaIntegration(myLambda), {
+            authorizer
         });
 
         new cdk.CfnOutput(this, 'myEndpointWithArgs', {
             description: 'output URL',
             value: `${endpoint.url}pdfbill`
+        });
+
+        new cdk.CfnOutput(this, 'UserPoolId', {
+            description: 'Cognito User Pool ID',
+            value: userPool.userPoolId
+        });
+
+        new cdk.CfnOutput(this, 'UserPoolClientId', {
+            description: 'Cognito User Pool Client ID',
+            value: userPoolClient.userPoolClientId
         });
     }
 }
